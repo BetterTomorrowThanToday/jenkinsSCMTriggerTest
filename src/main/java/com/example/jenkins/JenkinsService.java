c705e3880dc48597b398855501bd634eda80f206
김희찬 <92291084+BetterTomorrowThanToday@users.noreply.github.com>
2024-11-11 16:38:39 +0900

    fix) space

--- src/main/java/com/example/jenkins/JenkinsService.java
+++ src/main/java/com/example/jenkins/JenkinsService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class JenkinsService {
 
-    public int  hap(int n){
+    public int hap(int n){
         int result = 0;
         for(int i=1; i<=n; i++){
             result += i;
